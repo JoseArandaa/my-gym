9aaf15460d14622a06681fbb76a36717b01ea2a9
JoseArandaa <jose.aranda.et28@gmail.com>
2024-11-01 11:18:59 -0300

    feat(PricingCards): implement MercadoPago subscription service for packs

---
+++ src/components/PricingCards/onBuy.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { MercadoPagoConfig, PreApproval } from "mercadopago";
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MERCADO_PAGO_TOKEN!,
+});
+interface Pack {
+  name: string;
+  price: number;
+  description: string;
+  features: string[];
+}
+
+export async function onSubscribe(pack: Pack, usdPrice: number) {
+  try {
+    const dolarTransform = pack.price * (usdPrice || 1);
+
+    const subscription = await new PreApproval(client).create({
+      body: {
+        reason: `${pack.name} Subscription`,
+        auto_recurring: {
+          frequency: 1,
+          frequency_type: "months",
+          transaction_amount: dolarTransform,
+          currency_id: "ARS",
+          start_date: new Date().toISOString(),
+          end_date: new Date(
+            new Date().setFullYear(new Date().getFullYear() + 1)
+          ).toISOString(),
+        },
+        back_url:
+          "https://a737-2802-8010-4905-5201-444a-36c4-3398-8b1a.ngrok-free.app",
+        payer_email: "test_user_388127678@testuser.com",
+      },
+    });
+
+    return subscription.init_point;
+  } catch (error: any) {
+    console.error("Error creating subscription:", error);
+    throw new Error(`Subscription failed: ${error.message}`);
+  }
+}
