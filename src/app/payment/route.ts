770e4e69e90d324eb35cb97fa266ed292bca7b11
JoseArandaa <jose.aranda.et28@gmail.com>
2024-11-01 13:44:57 -0300

    feat(payment): create payment route

---
+++ src/app/payment/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+export async function POST(request: NextRequest) {
+  const body: unknown = await request.json();
+  console.log(body);
+  return Response.json({ success: true });
+}
